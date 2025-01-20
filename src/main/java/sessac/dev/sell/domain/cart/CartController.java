b3ef355898d11eb3256fe52ee6f9f25f165937e9
jungyoung <apollyon4@github.com>
2025-01-20 16:41:08 +0900

    local build success

--- src/main/java/sessac/dev/sell/domain/cart/CartController.java
+++ src/main/java/sessac/dev/sell/domain/cart/CartController.java
@@ -1,15 +1,12 @@
 package sessac.dev.sell.domain.cart;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import sessac.dev.sell.domain.cart.CartItemDto;
-import sessac.dev.sell.domain.cart.CartService;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -17,52 +14,53 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CartController {
 
-    private CartService cartService;
+	private CartService cartService;
 
-    @PostMapping("/add/{itemId}")
-    public String addToCart(@PathVariable Integer itemId, HttpServletRequest request) {
-        HttpSession session = request.getSession();
-        Long userId = (Long) session.getAttribute("id");
+	@PostMapping("/add/{itemId}")
+	public String addToCart(@PathVariable Integer itemId, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		Long userId = (Long) session.getAttribute("id");
 
-        System.out.println(userId);
-        System.out.println(itemId);
+		System.out.println(userId);
+		System.out.println(itemId);
 
-        CartItemDto cartItem = new CartItemDto();
-        cartItem.setMemberId(userId);
-        cartItem.setItemId(itemId);
-        System.out.println(cartItem.getItemId());
+		CartItemDto cartItem = new CartItemDto();
+		cartItem.setMemberId(userId);
+		cartItem.setItemId(itemId);
+		System.out.println(cartItem.getItemId());
 
-        cartService.addToCart(cartItem); // 장바구니에 추가
+		cartService.addToCart(cartItem); // 장바구니에 추가
 
-        return "redirect:/cart?memberId=" + userId; // 장바구니 페이지로 리다이렉트
-    }
+		return "redirect:/cart?memberId=" + userId; // 장바구니 페이지로 리다이렉트
+	}
 
+	@GetMapping
+	public void viewCart(HttpServletRequest request, Model model) {
+		HttpSession session = request.getSession();
+		Long memberId = (Long) session.getAttribute("id");
+		System.out.println(memberId);
+		List<CartItemDto> cartItems = cartService.getCartItems(memberId);
+		//        model.addAttribute("cartItems", cartItems);
+		//        return "/cart/cart"; // 장바구니 화면
+	}
 
-    @GetMapping
-    public void viewCart(HttpServletRequest request, Model model) {
-        HttpSession session = request.getSession();
-        Long memberId = (Long) session.getAttribute("id");
-        System.out.println(memberId);
-        List<CartItemDto> cartItems = cartService.getCartItems(memberId);
-//        model.addAttribute("cartItems", cartItems);
-//        return "/cart/cart"; // 장바구니 화면
-    }
+	@PostMapping("/update")
+	public String updateCartItem(@RequestParam Long memberId,
+			@RequestParam Integer itemId, @RequestParam int quantity) {
+		cartService.updateCartItemQuantity(memberId, itemId, quantity);
+		return "redirect:/cart?memberId=" + memberId; // 장바구니 페이지로 리다이렉트
+	}
 
-    @PostMapping("/update")
-    public String updateCartItem(@RequestParam Long memberId, @RequestParam Integer itemId, @RequestParam int quantity) {
-        cartService.updateCartItemQuantity(memberId, itemId, quantity);
-        return "redirect:/cart?memberId=" + memberId; // 장바구니 페이지로 리다이렉트
-    }
+	@PostMapping("/delete")
+	public String deleteCartItem(@RequestParam Long memberId,
+			@RequestParam Integer itemId) {
+		cartService.deleteCartItem(memberId, itemId);
+		return "redirect:/cart?memberId=" + memberId; // 장바구니 페이지로 리다이렉트
+	}
 
-    @PostMapping("/delete")
-    public String deleteCartItem(@RequestParam Long memberId, @RequestParam Integer itemId) {
-        cartService.deleteCartItem(memberId, itemId);
-        return "redirect:/cart?memberId=" + memberId; // 장바구니 페이지로 리다이렉트
-    }
-
-    @PostMapping("/clear")
-    public String clearCart(@RequestParam Long memberId) {
-        cartService.clearCart(memberId);
-        return "redirect:/cart?memberId=" + memberId; // 장바구니 페이지로 리다이렉트
-    }
+	@PostMapping("/clear")
+	public String clearCart(@RequestParam Long memberId) {
+		cartService.clearCart(memberId);
+		return "redirect:/cart?memberId=" + memberId; // 장바구니 페이지로 리다이렉트
+	}
 }
--- src/main/java/sessac/dev/sell/interceptor/LoginCheckInterceptor.java
+++ src/main/java/sessac/dev/sell/interceptor/LoginCheckInterceptor.java
@@ -1,28 +1,28 @@
 package sessac.dev.sell.interceptor;
 
-import sessac.dev.sell.domain.member.MemberResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import sessac.dev.sell.domain.member.MemberResponse;
 
 public class LoginCheckInterceptor implements HandlerInterceptor {
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+			Object handler) throws Exception {
 
-        // 1. 세션에서 회원 정보 조회
-        HttpSession session = request.getSession();
-        MemberResponse member = (MemberResponse) session.getAttribute("loginMember");
+		// 1. 세션에서 회원 정보 조회
+		HttpSession session = request.getSession();
+		MemberResponse member = (MemberResponse) session.getAttribute("loginMember");
 
-        // 2. 회원 정보 체크
-        if (member == null || member.getDeleteYn() == true) {
-            response.sendRedirect("/login.do");
-            return false;
-        }
+		// 2. 회원 정보 체크
+		if (member == null || member.getDeleteYn() == true) {
+			response.sendRedirect("/login.do");
+			return false;
+		}
 
-        return HandlerInterceptor.super.preHandle(request, response, handler);
-    }
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
 
 }
